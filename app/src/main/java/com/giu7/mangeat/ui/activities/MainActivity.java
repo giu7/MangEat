34db953ecfb4fbc19e9d8500bc0d7efd32b21c22
giuseppepiano <giuseppe.piano98@gmail.com>
2019-01-31 16:58:23 +0100

    Add first menu item

--- app/src/main/java/com/giu7/mangeat/ui/activities/MainActivity.java
+++ app/src/main/java/com/giu7/mangeat/ui/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package com.giu7.mangeat.ui.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.giu7.mangeat.R;
 
@@ -11,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.login_menu){
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        return true;
     }
 }
